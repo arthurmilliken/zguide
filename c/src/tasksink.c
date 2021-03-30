5016a91dc2806aedc54654ec1734a2bc9bddb206
Arthur Milliken <amilliken@gmail.com>
2021-03-29 22:26:29 -0700

    remove czmq invocation of pipeline demo

--- c/src/tasksink.c
+++ c/src/tasksink.c
@@ -38,34 +38,3 @@ int main (void)
     zmq_ctx_destroy (context);
     return 0;
 }
-
-// int main(int argc, char* argv[]) {
-// 	// Prepare our context and socket
-// 	zsock_t* receiver = zsock_new_pull("tcp://*:5558");
-// 	assert(receiver);
-
-// 	// Wait for start of batch
-// 	char* string = zstr_recv(receiver);
-// 	zstr_free(&string);
-
-// 	// Start our clock now
-// 	int64_t start_time = s_clock();
-
-// 	// Process 100 confirmation
-// 	int task_nbr;
-// 	for (task_nbr = 0; task_nbr < 100; task_nbr++) {
-// 		string = zstr_recv(receiver);
-// 		zstr_free(&string);
-// 		if (task_nbr % 10 == 0)
-// 			printf(":");
-// 		else
-// 			printf(".");
-// 		fflush(stdout);
-// 	}
-
-// 	// Calculate and report duration of batch
-// 	printf("Total elapsed time: %d msec\n", (int)(s_clock() - start_time));
-
-// 	zsock_destroy(&receiver);
-// 	return 0;
-// }
--- c/src/taskwork.c
+++ c/src/taskwork.c
@@ -33,30 +33,3 @@ int main (void)
     return 0;
 }
 
-// int main(int argc, char* argv[]) {
-// 	// Socket to receive messages on
-// 	// zsock_t* receiver = zsock_new_pull("tcp://localhost:5557");
-// 	// assert(receiver);
-// 	zsock_t* receiver = zsock_new(ZMQ_PULL);
-// 	assert(receiver);
-// 	int rc = zsock_connect(receiver, "tcp://localhost:5557");
-// 	assert(rc == 0);
-
-// 	// Socket to send messages to
-// 	zsock_t* sender = zsock_new_push("tcp://localhost:5558");
-// 	assert(sender);
-
-// 	// Process tasks forever
-// 	while (1) {
-// 		char* string = zstr_recv(receiver);
-// 		printf("%s.", string); // Show progress
-// 		fflush(stdout);
-// 		s_sleep(atoi(string)); // Do the work
-// 		zstr_free(&string);
-// 		zstr_send(sender, ""); // Send results to sink
-// 	}
-
-// 	zsock_destroy(&receiver);
-// 	zsock_destroy(&sender);
-// 	return 0;
-// }
