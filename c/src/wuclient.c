debc06bdf3e93eb23412cc3c9297a54ff8aaf5c5
Arthur Milliken <amilliken@gmail.com>
2021-04-08 18:47:24 -0700

    add valgrind and signal processing

--- c/src/wuclient.c
+++ c/src/wuclient.c
@@ -5,33 +5,57 @@
 //
 #include "zzz.h"
 
+static zsock_t* subscriber;
+static int update_nbr;
+
+static void cleanup(void) {
+    if (subscriber) {
+        zsock_destroy(&subscriber);
+        if (update_nbr < 100) {
+            s_console("C: %d updates processed.", update_nbr);
+        }
+        s_console("C: done.");
+    }
+}
+
+static void on_signal(int _) {
+    puts("");
+    s_console("C: interrupt received.");
+    cleanup();
+}
+
 int main(int argc, char* argv[]) {
     char* filter = (argc > 1) ? argv[1] : "10001";
     char* conn_url = (argc > 2) ? argv[2] : "tcp://localhost:5556";
 
     // Subscriber socket
-    zsock_t* subscriber = zsock_new_sub(conn_url, filter);
+    subscriber = zsock_new_sub(conn_url, filter);
     assert(subscriber);
     s_console("C: wuclient connected to %s", conn_url);
+
+    zsys_handler_set(on_signal);
     s_console("C: Collecting updates for '%s' from weather server...", filter);
 
     // Process 100 updates
-    int update_nbr;
     long total_temp = 0;
     for (update_nbr = 0; update_nbr < 100; update_nbr++) {
+        if (!subscriber) break;
         char* msg = zstr_recv(subscriber);
-
+        if (!msg) break;
         int zipcode, temperature, relhumidity;
         int rc = sscanf(msg, "%d %d %d", &zipcode, &temperature, &relhumidity);
         assert(rc > 0);
         total_temp += temperature;
         zstr_free(&msg);
     }
-    s_console(
-        "C: Average temperature for zipcode '%s' was %dF",
-        filter,
-        (int)(total_temp / update_nbr)
-    );
-    zsock_destroy(&subscriber);
+    if (update_nbr == 100) {
+        s_console(
+            "C: Average temperature for zipcode '%s' was %dF",
+            filter,
+            (int)(total_temp / update_nbr)
+        );
+    }
+
+    cleanup();
     return 0;
 }
--- c/src/wuserver.c
+++ c/src/wuserver.c
@@ -5,12 +5,29 @@
 //
 #include "zzz.h"
 
+static zsock_t* publisher;
+
+static void cleanup(void) {
+    if (publisher) {
+        zsock_destroy(&publisher);
+        s_console("S: done.");
+    }
+}
+
+static void on_signal(int _) {
+    puts("");
+    s_console("S: interrupt received.");
+    cleanup();
+}
+
 int main(int argc, char* argv[]) {
     // Publisher socket
     char bind_addr[] = "tcp://*:5556";
-    zsock_t* publisher = zsock_new_pub(bind_addr);
+    publisher = zsock_new_pub(bind_addr);
     assert(publisher);
     s_console("S: wuserver bound to %s", bind_addr);
+
+    zsys_handler_set(on_signal);
 
     while (true) {
         // Get values that will fool the boss
@@ -22,10 +39,10 @@ int main(int argc, char* argv[]) {
         // Send message to all subscribers
         char update[15];
         sprintf(update, "%05d %d %d", zipcode, temperature, relhumidity);
+        if (!publisher) break;
         int rc = zstr_send(publisher, update);
-        assert(rc == 0);
+        if (rc < 0) break;
     }
-    s_console("S: done");
-    zsock_destroy(&publisher);
+    cleanup();
     return 0;
 }
